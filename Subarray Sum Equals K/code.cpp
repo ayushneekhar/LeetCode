6b7863bedc0bdfd0782796d0b975c21f7c5c79ce
Ayush Neekhar <53595554+ayushneekhar@users.noreply.github.com>
2022-02-10 18:31:43 +0530

    Added Explaination to Subarray Sum= K

--- Subarray Sum Equals K/code.cpp
+++ Subarray Sum Equals K/code.cpp
@@ -1,3 +1,22 @@
+
+// Explaination
+
+// CumSum[j] - CumSum[i] = K  // This is the condition that we want to be able to satisft.
+
+// CumSum[j] = Cumilative sum at index j
+// CumSum[i] = Cumilative sum at index i
+
+// Array:       1    2    3   4    5     6    7     8     9
+// CumSum:      1    3    6   10   15   21    28   36    45
+//              --------------------------
+// 			    -----------------_________
+// 							     
+// k = 11
+
+// CumSum[5] (21) - CumSum[3] (10) = 11 = k // This case satisfies our condition
+
+// Rearranging our condition we get: CumSum[j] - k = CumSum[i]
+
 #include <bits/stdc++.h>
 
 using namespace std;
